9e241bb73461958e61f1ffa2e481e9c4bbc3a1c7
bytecod3 <emwiti658@gmail.com>
2024-11-14 19:07:38 +0300

    adds LCD working code

---
+++ software/onboard-device/include/defines.h
@@ -0,0 +1,11 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+/* LCD defines  */
+#define LCD_ADDR    0x27
+#define LCD_ROWS    4
+#define LCD_COLS    20
+
+#define BAUD        115200
+
+#endif
--- software/onboard-device/src/main.cpp
+++ software/onboard-device/src/main.cpp
@@ -1,9 +1,39 @@
+#include <Arduino.h>
+#include "defines.h"
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+/**
+ * @brief initalize the serial monitor
+ */
+void initSerial() {
+    Serial.begin(BAUD);
+}
+
+/**
+ * @brief initialize the LCD screen
+ * 
+ */
+void initLCD() {
+    lcd.init();
+    lcd.clear();
+    lcd.backlight(); // turn on backlight
+
+    // Welcome message
+    lcd.setCursor(4,0); // character 2 on line 0
+    lcd.print("Hello there!");
+
+    lcd.setCursor(6,1);
+    lcd.print("Welcome.");
+}
 
 void setup() {
+    initSerial();
+    initLCD();
 
 }
 
-
 void loop() {
-    
+
 }
