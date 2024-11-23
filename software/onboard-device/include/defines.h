a6495957dbaccbe28ee51235900359442d5aa675
bytecod3 <emwiti658@gmail.com>
2024-11-23 18:12:18 +0300

    updates GPS code

--- software/onboard-device/include/defines.h
+++ software/onboard-device/include/defines.h
@@ -1,12 +1,26 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
+#define DEBUG 1
+
+#if DEBUG
+#define debug(x)   Serial.print(x);
+#define debugln(x)  Serial.println(x);
+#define debugf(x, y) Serial.printf(x, y)
+
+#else 
+#define debug(x) 
+#define debugln(x)  
+#endif // END DEBUG
+
+
 /* LCD defines  */
 #define LCD_ADDR    0x27
 #define LCD_ROWS    4
 #define LCD_COLS    20
 
 #define BAUD        115200
+#define GPS_BAUD    9600
 
 
 /* LORA pins */
@@ -16,6 +30,7 @@
 #define NSS     4
 #define RESET   2
 #define DIO0    32
+#define LORA_FREQUENCY 868E6
 
 
 #endif
--- software/onboard-device/src/main.cpp
+++ software/onboard-device/src/main.cpp
@@ -1,21 +1,33 @@
 #include <Arduino.h>
 #include "defines.h"
 #include <LiquidCrystal_I2C.h>
+#include <TinyGPSPlus.h>
+#include <LoRa.h>
 
+HardwareSerial gpsSerial(2);
+TinyGPSPlus gps;
+float latitude, longitude;
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+void init_serial();
+void init_lcd();
+void init_lora();
+void get_gps();
+void update_lcd();
 
 /**
  * @brief initalize the serial monitor
  */
-void initSerial() {
+void init_serial() {
     Serial.begin(BAUD);
+    gpsSerial.begin(GPS_BAUD);
 }
 
 /**
  * @brief initialize the LCD screen
  * 
  */
-void initLCD() {
+void init_lcd() {
     lcd.init();
     lcd.clear();
     lcd.backlight(); // turn on backlight
@@ -31,16 +43,64 @@ void initLCD() {
 /**
  * @brief init LORA 
  */
-void initLORA() {
+void init_lora() {
+    LoRa.setPins(NSS, RESET, DIO0);
+    if(!LoRa.begin(LORA_FREQUENCY)) {
+        debugln("Lora failed to start");
+    } else {
+        debugln("Lora started OK!");
+    }
+}
 
+void get_gps() {
+    // Get location
+    while (gpsSerial.available() > 0) {
+        gps.encode(gpsSerial.read());
+
+        if (gps.location.isUpdated()) {
+            latitude = gps.location.lat();
+            longitude = gps.location.lng();
+
+            Serial.print("Latitude= "); 
+            Serial.print(latitude, 2);
+            Serial.print(" Longitude= "); 
+            Serial.println(longitude, 2);
+             
+
+            // time - in 24 hr format
+            // Hour (0-23) (u8) 
+            Serial.print(gps.time.hour());Serial.print(":");
+            // Minute (0-59) (u8)
+            Serial.print(gps.time.minute());Serial.print(":");
+            // Second (0-59) (u8)
+            Serial.print(gps.time.second());
+            Serial.println();
+            // 100ths of a second (0-99) (u8)
+        } 
+    }
+}
+
+void update_lcd() {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Lat:");
+    lcd.print(latitude);
+    lcd.setCursor(0,1);
+    lcd.print("Long:");
+    lcd.print(longitude);
+    delay(2000);
+    lcd.clear();
 }
 
 void setup() {
-    initSerial();
-    initLCD();
-    initLORA();
+    init_serial();
+    init_lcd();
+    init_lora();
 }
 
 void loop() {
+
+    get_gps();
+    update_lcd();
 
 }
